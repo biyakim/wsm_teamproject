b90a529ec7fa0dcb7afffeff0ca75589c6959238
biyakim <w2116@e-mirim.hs.kr>
2022-11-17 20:08:22 +0900

    두번째 수정 반응형 함

---
+++ mainpage.js
@@ -0,0 +1,6 @@
+const barbtn = document.querySelector('.bar');
+const menu = document.querySelector('.navbar_menu');
+
+barbtn.addEventListener('click',()=>{
+    menu.classList.toggle('avtive');
+});
