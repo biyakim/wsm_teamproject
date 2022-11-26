aad8b206e1df67dafeda59a81397937066547f6c
biyakim <w2116@e-mirim.hs.kr>
2022-11-26 15:26:13 +0900

    오류 수정

--- js/login_join.js
+++ js/login_join.js
@@ -1,6 +1,6 @@
 function login(){
-    var id = document.querySelector("#id");
-    var pw = document.querySelector('#pw');
+    var id = document.querySelector(".id");
+    var pw = document.querySelector('.pw');
 
     if(id.value == "" || pw.value == "") {
         alert("로그인을 할 수 없습니다")
@@ -13,9 +13,9 @@ function back(){
     history.go(-1);
 }
 function create_id(){
-    var id = document.querySelector('#id');
-    var pw = document.querySelector('#pw');
-    var r_pw = document.querySelector('#r_pw');
+    var id = document.querySelector('.id');
+    var pw = document.querySelector('.pw');
+    var r_pw = document.querySelector('.r_pw');
 
     if(id.value == "" || pw.value == "" || r_pw.value == ""){
         alert("회원가입을 할 수 없습니다")
