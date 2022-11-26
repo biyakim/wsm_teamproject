ecc92262ea626c9bf153c52731ea11ccb2a79db3
biyakim <w2116@e-mirim.hs.kr>
2022-11-26 14:12:27 +0900

    로그인 회원가입 글쓰는 페이지 js

---
+++ js/login.js
@@ -0,0 +1,7 @@
+const barbtn = document.querySelector('.bar');
+const menu = document.querySelector('.navbar_menu');
+
+barbtn.addEventListener('click',()=>{
+    menu.classList.toggle('avtive');
+});
+
