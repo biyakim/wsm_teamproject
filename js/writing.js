33ef01cc178271ce623e30a349b483da5bb37faf
biyakim <w2116@e-mirim.hs.kr>
2022-11-28 12:19:51 +0900

    api 해보기

--- js/writing.js
+++ js/writing.js
@@ -10,6 +10,15 @@ document.addEventListener('DOMContentLoaded', () => {
 const selectDrop = document.querySelector('#countires');
 // const selectDrop = document.getElementById('countires');
 
-fetch('')
+fetch('https://www.career.go.kr/cnet/openapi/getOpenApi?apiKey=922f4bc8842956d94e130993d069759b&svcType=api&svcCode=MAJOR&contentType=json&gubun=univ_list').then(res => {
+    return res.json(); 
+}).then(data => {
+    let output = "";
+   data.forEach(country => {
+    output += `<options>${country.mClass}</options>`;
+   })
+}).catch(err => {
+    console.log(err);
+})
 
 })
