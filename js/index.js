703040ec6a516dfa63b088d3fbb1c48e2f924ed0
hajin <w2128@e-mirim.hs.kr>
2022-11-28 12:17:28 +0900

    api키로 url을 만들어서 넘겨주는 handler ( ) 시작

--- js/index.js
+++ js/index.js
@@ -4,3 +4,7 @@ const menu = document.querySelector('.navbar_menu');
 barbtn.addEventListener('click',()=>{
     menu.classList.toggle('avtive');
 });
+
+const handler = () => {
+    const API_KEY = "";
+}
